f183fc73a0a5401a4dd4dd4046cd35a6827f5e58
Esdras de Melo Lima <dm.esdras@gmail.com>
2024-06-19 20:21:50 -0300

    Initial Commit

---
+++ src/app/dashboard/line-chat.tsx
@@ -0,0 +1,3 @@
+export default function LineChat() {
+    return <h1>Line chart</h1>;
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+function BarChat() {
+    return <h1>Bar chart</h1>;
+}
+
+export default function Dashboard() {
+    return <h1>Dashboard</h1>;
+}
